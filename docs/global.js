3504e95000b77c2144239c4f55d309b376cc77af
mauro 🤙 <mauro.balades@tutanota.com>
2024-04-15 20:34:02 +0000

    Started working on page contents

---
+++ docs/global.js
@@ -0,0 +1,65 @@
+
+const pages = {
+    "": {
+        title: "Home",
+        description: "Welcome to crafting compilers!"
+    },
+    "introduction": {
+        "about": {
+            title: "About Elysian",
+            description: "Learn about the Elysian programming language."
+        },
+    },
+}
+
+function renderSidebar(page, otherPages = []) {
+    let sidebar = document.createElement("div");
+    sidebar.id = "sidebar";
+    let sidebarContent = document.createElement("div");
+    sidebarContent.id = "sidebar-content";
+    let sidebarTitle = document.createElement("h1");
+    sidebarTitle.id = "sidebar-title";
+    sidebarTitle.innerHTML = "Crafting Compilers";
+    sidebarContent.appendChild(sidebarTitle);
+    let sidebarDescription = document.createElement("p");
+    sidebarDescription.id = "sidebar-description";
+    sidebarDescription.innerHTML = "A guide to crafting compilers.";
+    sidebarContent.appendChild(sidebarDescription);
+    let sidebarPages = document.createElement("ul");
+    sidebarPages.id = "sidebar-pages";
+    for (let page in pages) {
+        let sidebarPage = document.createElement("li");
+        sidebarPage.id = "sidebar-page";
+        sidebarPage.innerHTML = page.title;
+        sidebarPages.appendChild(sidebarPage);
+    }
+    sidebarContent.appendChild(sidebarPages);
+    sidebar.appendChild(sidebarContent);
+    document.body.appendChild(sidebar);
+}
+
+function renderPage(page, otherPages = []) {
+    document.title = page.title;
+    renderSidebar();
+    if (typeof window.pageContents !== "undefined") {
+        let content = document.createElement("div");
+        content.id = "content";
+        var converter = new showdown.Converter(),
+        html      = converter.makeHtml(window.pageContents);
+        content.innerHTML = html;
+        document.body.appendChild(content);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    let path = window.location.pathname.split("/");
+    if (path.length == 0) {
+        path = "";
+    }
+
+    let page = pages[path[0]];
+    if (page != "") {
+        return renderPage(page[path[1]]);
+    } 
+    return renderPage(page);
+});
